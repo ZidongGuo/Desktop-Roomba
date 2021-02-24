2916ee1ac55eb49bb0b9627504ca743461494ac0
Zidong Guo <zg753@nyu.edu>
2021-02-24 09:59:39 -0800

    #5 Write Stub Functions in demo.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='Desktop_Roomba',
+setup(name='DesktopRoomba',
       version='1.0',
       description='Project Stub Library',
       author='Zidong Guo',
       author_email='zg753@nyu.edu',
       url='https://github.com/ZidongGuo/Desktop-Roomba/',
-      py_modules=['Desktop_Roomba'],
+      py_modules=['DesktopRoomba'],
 )
 
