ea9a2ccaf52a2cc1cab2b8a5dd3685505fc9129b
Zidong Guo <zg753@nyu.edu>
2021-02-24 09:30:41 -0800

    #5 Write Stub Functions in demo.py

---
+++ DesktopRoomba.py
@@ -0,0 +1,38 @@
+def setup():
+    print("All programs have been set up successfully!")
+    return True
+
+def Read_Ultrasonic_Distance():
+    print("Distance from the ultrasonic sensors is ")
+
+def Read_IR_Reflectance():
+    print("The reflectance is ")
+
+def Setup_IMU():
+    print("IMU is set up")
+
+def Read_Angle():
+    print("Turning Angle is")
+
+def Read_Acceleration():
+    print("Acceleration is ")
+
+def Set_DutyCycle():
+    print("Duty Cycle is set to be")
+
+def Set_PWM_Frequency():
+    print("PWM is set to be")
+
+def Away_from_edges():
+    print("Move away from edges")
+
+def Turn_Left():
+    print("Turn Left")
+def Turn_Right():
+    print("Turn Right")
+
+def Forward():
+    print("go forward")
+
+def Backward():
+    print("go backward")
---
+++ demo.py
@@ -0,0 +1,22 @@
+import DesktopRoomba
+
+DesktopRoomba.setup()
+
+DesktopRoomba.Read_Ultrasonic_Distance()
+
+DesktopRoomba.Read_IR_Reflectance()
+
+DesktopRoomba.Setup_IMU()
+DesktopRoomba.Read_Angle()
+DesktopRoomba.Read_Acceleration()
+
+
+DesktopRoomba.Set_DutyCycle()
+DesktopRoomba.Set_PWM_Frequency()
+DesktopRoomba.Away_from_edges()
+DesktopRoomba.Turn_Left()
+DesktopRoomba.Turn_Right()
+DesktopRoomba.Forward()
+DesktopRoomba.Backward()
+
+
