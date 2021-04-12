b6e7b41f797ebf5cff43f44aa758f51d7e834e76
Zidong Guo <zg753@nyu.edu>
2021-04-12 11:01:19 -0700

    #13 Test two DC motors and the motor driver

--- DesktopRoomba.py
+++ DesktopRoomba.py
@@ -2,6 +2,8 @@ import time
 import board
 import busio
 from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
+import RPi.GPIO as GPIO
+from time import sleep
 
 def setup():
     print("All programs have been set up successfully!")
@@ -42,7 +44,25 @@ def Turn_Right():
     print("Turn Right")
 
 def Forward():
+    GPIO.setmode(GPIO.BOARD)
+    Motor1A = 16
+    Motor1B = 18
+    Motor1E = 22
+    Motor2A = 23
+    Motor2B = 21
+    Motor2E = 19
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1E,GPIO.OUT)
+    GPIO.setup(Motor2A,GPIO.OUT)
+    GPIO.setup(Motor2B,GPIO.OUT)
+    GPIO.setup(Motor2E,GPIO.OUT)
     print("go forward")
-
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    GPIO.output(Motor2A,GPIO.HIGH)
+    GPIO.output(Motor2B,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.HIGH)
 def Backward():
     print("go backward")
