a810c2de4d54134457fe09501c670b612186d45a
Zidong Guo <zg753@nyu.edu>
2021-04-26 03:26:54 -0700

    Add a code for testing IR sensors

---
+++ IRTest.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+GPIO.setmode(GPIO.BCM)
+GPIO_ir =1
+try:
+
+  while True:
+
+    # let capacitor discharge
+    GPIO.setup(GPIO_ir,GPIO.OUT)
+    GPIO.output(GPIO_ir, GPIO.HIGH)
+    # let it discharge allll the way
+    time.sleep(1) 
+    GPIO.setup(GPIO_ir,GPIO.IN)
+    start=time.time()
+    GPIO.wait_for_edge(GPIO_ir, GPIO.FALLING)
+    end=time.time()
+    print("%f seconds, %f us" %(end-start,1000000*(end-start) ) )
+
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  sys.exit()
