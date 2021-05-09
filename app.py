9fa3b753d86ced9052649bfea733bfeb5818dadc
ZidongGuo <zg753@nyu.edu>
2021-05-09 18:34:30 +0100

    #10 copy code from TestApp.py to App.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ DR.setup()
 MinDistLR=5 #all four read_distance() should return a value less than 3
 MinDistF=7
 MaxTime=800 #Read_IR_Reflectance() should return a value less than 800
-Mode=1
+Mode=2
 
 def FreeToGo():
     if (DR.Read_DistanceL()>MinDistLR and DR.Read_DistanceR()>MinDistLR and DR.Read_DistanceF()>MinDistF and DR.Power==1):
@@ -21,6 +21,7 @@ def Readytostart():
     if (DR.Power==1):
         return True
     return False
+
 try:
     while True:
         wait(lambda: Readytostart())
@@ -29,8 +30,6 @@ try:
             if (DR.Power==0):
                 break
             while FreeToGo()==1:
-                if (DR.Power==0):
-                    break
                 DR.Forward(40)
                 sleep(0.03)
             while (DR.Read_DistanceF()<=MinDistF):
@@ -39,8 +38,8 @@ try:
                 DR.Stop()
                 sleep(0.02)
                 if (DR.Read_DistanceL()<MinDistLR and DR.Read_DistanceR()<MinDistLR):
-                    DR.Back(40)
-                    sleep(0.2)
+                    DR.Backward(40)
+                    sleep(0.5)
                     RandAngle=random.random()
                     DR.Turn_Right(RandAngle)
                 elif (DR.Read_DistanceL()<MinDistLR):
@@ -61,7 +60,43 @@ try:
 
                 RandAngle=random.random()
                 DR.Turn_Left(RandAngle)
+        while Mode==2:
+            if (DR.Power==0):
+                break
 
+            tourtime=3
+            increasedtime=0.5
+            numberofspiral=10
+            minspeed=30
+            maxspeed=90
+            for i in range (0, numberofspiral):
+                if (DR.Power==0):
+                    break
+                if (FreeToGo()==0):
+                    while (FreeToGo()==0):
+                        DR.Stop()
+                        sleep(0.5)
+                        if (DR.Read_DistanceF()<=MinDistF and DR.Read_DistanceL()<MinDistLR and DR.Read_DistanceR()<MinDistLR):
+                            DR.Back(40)
+                            sleep(0.5)
+                        RandAngle=random.random()
+                        DR.Turn_Right(RandAngle)
+                    DR.Forward(50)
+                    RandTime=3*random.random()
+                    sleep(RandTime)
+                    break
+                DR.pwm1.start(maxspeed)
+                DR.pwm2.start(minspeed)
+                GPIO.output(DR.Motor1A,GPIO.HIGH)
+                GPIO.output(DR.Motor1B,GPIO.LOW)
+                GPIO.output(DR.Motor1E,GPIO.HIGH)
+
+                GPIO.output(DR.Motor2A,GPIO.HIGH)
+                GPIO.output(DR.Motor2B,GPIO.LOW)
+                GPIO.output(DR.Motor2E,GPIO.HIGH)
+                sleep(tourtime)
+                minspeed=minspeed+5
+                tourtime=tourtime+increasedtime
+                print("Spiral: ", i);
 except KeyboardInterrupt:
     GPIO.cleanup()
-
